1699d4fd12708dd7246f313dd113904bc05db816
ricardo.simoes <ricardo.simoes@edulabzz.com.br>
2020-05-23 20:24:43 -0300

    codigo identado

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const repositories = [];
 
 //Rota que mostra todos os repositorios
 app.get("/repositories", (request, response) => {
-  return response.json ( repositories )
+  return response.json(repositories)
 });
 
 //Rota que cadastra um novo repositorio passando title, url e techs, gerando id automatico e like 0
@@ -22,9 +22,9 @@ app.post("/repositories", (request, response) => {
 
   const project = { id: uuid(), title, url, techs, likes: 0 }
 
-  repositories.push( project )
+  repositories.push(project)
 
-  return response.json( project )
+  return response.json(project)
 });
 
 //Rota que altera um repositorio via id sendo possivel apenas alterar title, url e techs
@@ -32,37 +32,37 @@ app.put("/repositories/:id", (request, response) => {
   const { title, url, techs } = request.body
   const { id } = request.params
 
-  let repository = repositories.find( repository => repository.id === id )
+  let repository = repositories.find(repository => repository.id === id)
   if (!repository)
     return response.status(400).json({ error: "Repository not found!" })
 
-  repository = {...repository, title, url, techs}
+  repository = { ...repository, title, url, techs }
 
-  return response.json( repository )
+  return response.json(repository)
 });
 
 //Rota que exlui um repositorio por id
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params
 
-  const repositoryIndex = repositories.findIndex( repository => repository.id === id )
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id)
   if (repositoryIndex < 0)
-    return response.status(400).json({ error: "Repository not found!"})
+    return response.status(400).json({ error: "Repository not found!" })
 
-    repositories.splice(repositoryIndex, 1)
+  repositories.splice(repositoryIndex, 1)
 
-    return response.status(204).send()
-  
+  return response.status(204).send()
+
 });
 
 //Rota que cadastra likes no repositorio de 1 em 1, optei pelo put porém da pra usar post.
 app.put("/repositories/:id/like", (request, response) => {
   const { id } = request.params
 
-  const repository = repositories.find( repository => repository.id === id )
+  const repository = repositories.find(repository => repository.id === id)
 
   if (!repository)
-    return response.status(400).json({ error: "Repository not found!"})
+    return response.status(400).json({ error: "Repository not found!" })
 
   repository.likes += 1
 
@@ -73,10 +73,10 @@ app.put("/repositories/:id/like", (request, response) => {
 app.delete("/repositories/:id/like", (request, response) => {
   const { id } = request.params
 
-  const repository = repositories.find( repository => repository.id === id )
+  const repository = repositories.find(repository => repository.id === id)
 
   if (!repository)
-    return response.status(400).json({ error: "Repository not found!"})
+    return response.status(400).json({ error: "Repository not found!" })
 
   repository.likes -= 1
 
